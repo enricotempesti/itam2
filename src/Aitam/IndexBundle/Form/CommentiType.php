94ad30bb5706e267a85ef8db08fed5ca7f98bf7b
enricotempesti <entemp@tin.it>
2013-06-06 21:06:05 +0200

    aggiornato build form

--- src/Aitam/IndexBundle/Form/CommentiType.php
+++ src/Aitam/IndexBundle/Form/CommentiType.php
@@ -4,10 +4,11 @@ namespace Aitam\IndexBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilder;
+use Symfony\Component\Form\FormBuilderInterface; 
 
 class CommentiType extends AbstractType
 {
-    public function buildForm(FormBuilder $builder, array $options)
+    public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
             ->add('utente')
